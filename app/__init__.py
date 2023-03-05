47c642a132cecb0bc32735b5c81888e74c5ff898
Varis Nijat <vnijat93@gmail.com>
2023-03-05 18:24:06 -0500

    DEV Add more scripts

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import config
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = "auth.login"
 
 
 def create_app(config_name):
@@ -12,10 +17,16 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
+    db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
 
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     return app
